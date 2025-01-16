28ccf715d5e3963fc7e93174733e52a1ff513b87
Kristen Cabrera <cabrera.kristen@gmail.com>
2025-01-16 18:14:21 +0100

    test tailwind

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import type { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>My Project</span>,
+	project: {
+		link: "https://github.com/shuding/nextra-docs-template",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	footer: {
+		text: "Nextra Docs Template",
+	},
+};
 
-export default config
+export default config;
